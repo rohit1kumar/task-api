289f3d6e072bec926b35626028c9057ad38d98aa
Rohit Kumar <kumar1rohit@outlook.com>
2025-01-18 13:01:18 +0530

    Initial setup

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default {
+	DATABASE_URL: process.env.DATABASE_URL,
+	PORT: process.env.PORT || 8000,
+	PASSWORD_HASH_SALT: process.env.PASSWORD_HASH_SALT || 10,
+	JWT_SECRET: process.env.JWT_SECRET || 'top-secret',
+	JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d'
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import config from './config.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
+app.listen(config.PORT, () => {
+	console.log(`Server running on port ${config.PORT}`)
+})
